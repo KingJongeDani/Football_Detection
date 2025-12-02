93dc346ea42e48bc1a27f2a3dcad8c73b082a606
Daniel <tvcornflakes@gmail.com>
2025-12-02 16:36:30 +0100

    Football_Detection v1.8

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ def stream() -> Generator[bytes, None, None]:
         if ok:
             #Verwandelt funktion stream() in einen Generator welcher dann Frame für Frame die predictede Bilder an die Website geben kann, damit es weniger Wartezeit gibt, dass man etwas sieht
             # Problem: Es laggt dadurch, es wird nicht mehr "Maßstabsgetreu" (1sek != 1sek)
+            # Kurz gesagt "pausiert" yield die Funktion an einer Stelle und kann sie genau dort wieder weiterführen
             yield (b'--frame\r\nContent-Type: image/jpeg\r\n\r\n' + jpg.tobytes() + b'\r\n') 
     cap.release()
 
@@ -83,7 +84,12 @@ def download_and_cut_youtube(url: str, start: str, end: str, output_path: Path):
 @ui.page('/')
 def index():
     ui.markdown('#Football Detection')
-    ui.label('Analysiere einen YouTube-Ausschnitt mit YOLO:')
+    ui.markdown('##Analysiere einen YouTube-Ausschnitt mit YOLO:')
+    ui.markdown("1.) Youtube Fußball Video mit Folgelperspektive von der Seitenlinie suchen")
+    ui.markdown("2.) Link reinkopieren und Start- / Stoppzeit eingeben (mm:ss) - maximal 15 Sekunden Differenz ;  Video so kurz wie möglich wählen")
+    ui.markdown("3.) Auf Clip Analysieren drücken und warten bis das Video geladen ist")
+    ui.markdown("4.) Auf Zum Video drücken und genießen! :)")
+
 
     link_input = ui.input('YouTube Link')
     start_input = ui.input('Startzeit (mm:ss)', value='00:00')
@@ -92,19 +98,34 @@ def index():
     # Spinner Ladebalken
     spinner = ui.spinner(size='lg').classes('mt-4').style('display: none;')
 
+    # Button "Zum Video" wird später eingefügt
+    video_button = ui.button(
+        'Zum Video',
+        on_click=lambda: ui.run_javascript('window.location.href="/video"')
+    ).classes('mt-4')
+    video_button.disable()   # Startzustand: grau/deaktiviert
+
     async def handle_youtube():
         spinner.style('display: block;')   # Spinner einblenden
         ui.notify('Neues YouTube-Video wird heruntergeladen...')
         await asyncio.sleep(0.5)
 
-        download_and_cut_youtube(link_input.value, start_input.value, end_input.value, uploaded_video_path)
+        try:
+            download_and_cut_youtube(link_input.value, start_input.value, end_input.value, uploaded_video_path)
+            ui.notify('Clip gespeichert!')
+            video_button.enable()   # Aktiviert den Button, sobald Video fertig ist
+        except Exception as e:
+            ui.notify('Es gab einen Fehler bei der Eingabe. Lade die Website neu und versuche es erneut.', type='warning')
+            print(f'Fehler: {e}')
+        finally:
+            spinner.style('display: none;')    # Spinner ausblenden
+            await asyncio.sleep(0.5)
 
-        ui.notify('Clip gespeichert!')
-        spinner.style('display: none;')    # Spinner ausblenden
-        await asyncio.sleep(0.5)
-
+    # Erst der Prediction-Button
     ui.button('YouTube-Clip analysieren', on_click=handle_youtube).classes('mt-2')
-    ui.button('Zum Video', on_click=lambda: ui.run_javascript('window.location.href="/video"')).classes('mt-4')
+
+    # Danach der "Zum Video"-Button
+    video_button
 
 
 # Video-Seite, bekommt das Video von anderen Endpunkt (/yolo_stream)
@@ -122,7 +143,7 @@ def video_page():
                 status_label.text = 'Video zu Ende'
                 break
 
-    ui.timer(1.0, check_done)  # prüft jede Sekunde
+    ui.timer(1.0, check_done)  # prüft jede Sekunde, also fragt eigentlich ab "Ist es schon fertig?"
     ui.button('Zurück zur Startseite', on_click=lambda: ui.run_javascript('window.location.href="/"')).classes('mt-4')
 
 ui.run(host='0.0.0.0', port=9000, reload=False)
